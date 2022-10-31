3ceb2c3b05afa9d35fce750a333e8626e4aa42e1
Ville Toikkanen <vtoikkanen@hotmail.fi>
2022-10-31 04:31:37 +0200

    Updates

--- src/data/letters.js
+++
@@ -1,2 +0,0 @@
-export const letters_en = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "W", "V", "X", "Y", "Z"];
-export const letters_fi = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "W", "V", "X", "Y", "Z", "Å", "Ä", "Ö"];
